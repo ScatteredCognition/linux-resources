835c2f86dc9100828b126040709063609c73642c
Faeiz Mahrus <faeizmahrus@outlook.com>
2025-09-29 18:12:13 +0600

    fix: misc fixes

--- exp/sb-factory-reset.sh
+++ exp/sb-factory-reset.sh
@@ -6,18 +6,16 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-mkdir /var/sb-fr/ 2>/dev/null
-
 # See if user initialized a factory reset
 # Ensure /etc/fstab and /etc/crypttab exist, restore from /var/sb-fr if missing
 if [ ! -f /etc/fstab ] && [ -f /var/sb-fr/sb-factory-reset ]; then
   echo "/etc/fstab not found."
-  echo "Factory restore was initiated."
   read -rp "Restore /etc/fstab from /var/sb-fr/fstab? (y/N): " ans
   case "$ans" in
     y|Y )
-      echo "Restoring /etc/fstab from /var/sb-fr/fstab."
       cp /var/sb-fr/fstab /etc/fstab
+      echo "Restored /etc/fstab from /var/sb-fr/fstab."
+      echo -e "Reboot the system."
       exit 1
       ;;
     * )
@@ -28,12 +26,12 @@ if [ ! -f /etc/fstab ] && [ -f /var/sb-fr/sb-factory-reset ]; then
 fi
 if [ ! -f /etc/crypttab ] && [ -f /var/sb-fr/sb-factory-reset ]; then
   echo "/etc/crypttab not found."
-  echo "Factory restore was initiated."
   read -rp "Restore /etc/crypttab from /var/sb-fr/crypttab? (y/N): " ans
   case "$ans" in
     y|Y )
-      echo "Restoring /etc/crypttab from /var/sb-fr/crypttab."
       cp /var/sb-fr/crypttab /etc/crypttab
+      echo "Restored /etc/crypttab from /var/sb-fr/crypttab."
+      echo -e "Reboot the system."
       exit 1
       ;;
     * )
@@ -56,6 +54,7 @@ echo -e "All user accounts and data will be lost."
 read -rp "Do you want to continue? (y/N): " choice
 case "$choice" in
   y|Y ) echo "Proceeding with factory reset..."
+  mkdir /var/sb-fr/ 2>/dev/null
   touch /var/sb-fr/sb-factory-reset
   ;;
   * ) echo "Aborting."; exit 1;;
@@ -77,4 +76,6 @@ cp /etc/crypttab /var/sb-fr/crypttab
 # Run ostree command
 echo -e "Resetting all rpm-ostree overrides and overlays and redeploying system..."
 ostree admin deploy "$( rpm-ostree status | grep -m 1 -P '^(?=.*fedora:fedora)(?=.*silverblue)' | awk '{$1=$1; print}' )" --no-merge --karg-proc-cmdline
-echo -e "Factory reset complete. Please rerun this script after rebooting into the new deployment."
+echo -e "Factory reset complete."
+echo -e "Please rerun this script to restore /etc/fstab and /etc/crypttab"
+echo -e "Reboot the system."
